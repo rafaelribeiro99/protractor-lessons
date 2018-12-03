51dbcf4211d8cf560d8a455174c5a7f6bb034293
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2018-12-03 15:00:30 -0300

    First Commit Protractor

---
+++ ProtractorTutorial/Spec1.js
@@ -0,0 +1,22 @@
+/**
+ * Autor: Rafael Luna Ribeiro - Estudos Protractor
+ */
+
+//primeiro parametro do describe é o nome da Test Suite
+//Segundo parametro é a function, que vai ter todos os test cases, vários blocos It
+describe('Protractor primeiro teste', function() {
+	
+	//IT: primeiro parametro é o nome do test case
+	it('Abrir site do Angular', function() {
+		//codigo protractor
+		
+	})
+	
+	it('Fechar o navegador', function() {
+		//codigo protractor
+		
+	})
+	
+})
+
+
