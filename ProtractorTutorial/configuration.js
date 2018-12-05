17e8b120b67e458e003dca742b02c2df512f9445
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2018-12-05 13:11:59 -0300

    Adding Protractor folder in the project

---
+++ ProtractorTutorial/configuration.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Rafael Luna Ribeiro - Protractor Studies
+ */
+
+exports.config = {
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	specs: ['Specs/Spec1.js'],
+    //directConnect: true
+		
+};
