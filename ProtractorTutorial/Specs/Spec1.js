ac7bf4643945af202393fdf39eb8d8b110da860e
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2018-12-05 13:34:01 -0300

    Changed the Test Cases from Spec1 Test Suite

--- ProtractorTutorial/Specs/Spec1.js
+++ ProtractorTutorial/Specs/Spec1.js
@@ -7,17 +7,19 @@
 describe('First Protractor Test Suite', function() {
 	
 	//IT: 1st parameter: Name of the Test Case
-	it('Open Angular website', function() {
+	it('Open Angular website and access Getting Started', function() {
 		//protractor code
 		//This command will open the website on the parameter
 		browser.get('https://angular.io');
 		//element(by.partialLinkText('quickstart')).click();
-		browser.sleep(5000);
-		
+		browser.sleep(3000);
+		element(by.xpath('//*[@id="intro"]/div[2]/a')).click();
+		browser.sleep(3000);
+		element(by.id('getting-started')).isDisplayed();
 		
 	})
 	
-	it('Close browser', function() {
+	it('Close the browser', function() {
 		//protractor code
 		browser.close();
 		
