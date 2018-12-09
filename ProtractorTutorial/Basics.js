225cb966a3d9fc07bc48327d12c4c127378b3e6d
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2018-12-09 19:51:50 -0300

    Basics of JavaScript Lessons

---
+++ ProtractorTutorial/Basics.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Rafael Luna Ribeiro - Protractor Studies
+ */
+
+console.log("HELLO WORLD");
+
+var nome = "Rafael";
+var idade = 28;
+
+console.log(nome);
+console.log(idade);
+console.log("My name is "+nome+" and i am "+idade+" yeasr old");
+
+
+if(nome=="Rafaell"){
+	console.log("Yes, my name is "+nome);
+}else if(idade==29){
+	console.log("Yes, my age is "+idade);
+} else{
+	console.log("You don't know my name and age");
+}
---
+++ ProtractorTutorial/Basics2.js
@@ -0,0 +1,24 @@
+/**
+ * Author: Rafael Luna Ribeiro - Protractor Studies
+ */
+
+//Lerning Loops with For
+//Print numbers from 1 to 100
+
+for(var i = 1; i <= 100; i++ ){
+	console.log("Number: "+i);
+}
+
+for(var i = 1; i <= 100; i+2 ){
+	console.log("Number: "+i);
+}
+
+for(var i = 1; i <= 100; i=i+5 ){
+	console.log("Number: "+i);
+}
+
+var j=5;
+while(j>5){
+	console.log(j);
+	j++;
+}
