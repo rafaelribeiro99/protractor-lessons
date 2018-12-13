179fe56b95a2daad1e6bf379e873d0f7bcfa4897
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2018-12-12 22:58:57 -0300

    Locators and Assertions

--- ProtractorTutorial/ElementBasics.js
+++ ProtractorTutorial/ElementBasics.js
@@ -2,6 +2,46 @@
  * Author: Rafael Luna Ribeiro - Protractor Studies
  */
 
-describe(function(){
+describe('Protractor Element Demo', function(){
+	var width = 1930;
+	var height = 1030;
+	var x = 1;
+	var y = 1;
 	
+	it('ADDITION Operation', function(){
+		
+		browser.get('http://juliemr.github.io/protractor-demo/');
+		browser.driver.manage().window().setPosition(x, y);
+		browser.driver.manage().window().setSize(width, height);
+		
+		element(by.model("first")).sendKeys("3");
+		element(by.model("second")).sendKeys("2");
+		element(by.model("operator")).click();
+		element(by.css("option[value='ADDITION']")).click();
+		element(by.id("gobutton")).click();
+		
+		//FOR ASSERTIONS, JASMINE TAKES CARE OF RESOLVING PROMISE(ASYNCRONOUS PROBLEM)
+		expect(element(by.css("h2[class='ng-binding']")).getText()).toBe("5");
+//		element(by.css("h2[class='ng-binding']")).getText().then(function(text){
+//			console.log(text);
+//		})
+		//this command will run FIRST, because it needs you to resolve promise
+		//2 potencial problems: 1.sequence is not guarenteed
+		//tagname[attribute='value']
+		
+	})
+	
+	it('SUBTRACTION Operation', function(){
+		browser.get('http://juliemr.github.io/protractor-demo/');
+		browser.driver.manage().window().setPosition(x, y);
+		browser.driver.manage().window().setSize(width, height);
+		
+		element(by.model("first")).sendKeys("10");
+		element(by.model("operator")).click();
+		element(by.css("option[value='SUBTRACTION']")).click();
+		element(by.model("second")).sendKeys("2");
+		element(by.id("gobutton")).click();
+		expect(element(by.css("h2[class='ng-binding']")).getText()).toBe("8");
+	})
+		
 })
--- ProtractorTutorial/configuration.js
+++ ProtractorTutorial/configuration.js
@@ -4,7 +4,19 @@
 
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['Specs/Spec1.js'],
-    //directConnect: true
+	
+	//specs: ['Specs/Spec1.js'],
+	specs: ['ElementBasics.js'],
+    
+	capabilities: {
+		'browserName': 'chrome'
+	    //,'browserName': 'firefox'
+	    //,'browserName': 'internet explorer'
+		//,'browserName': 'MicrosoftEdge'	
+		
+	}
+
+   //directConnect: true
+    
 		
 };
