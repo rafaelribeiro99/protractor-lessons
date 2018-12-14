e286e8bb07ec00cf24074c18e5aef26c620c0df8
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2018-12-13 22:04:18 -0300

    First Element All lesson

---
+++ ProtractorTutorial/All.js
@@ -0,0 +1,42 @@
+/**
+ * Author: Rafael Luna Ribeiro - Protractor Studies
+ */
+
+describe('Chain Locators', function(){
+	var width = 1930;
+	var height = 1030;
+	var x = 1;
+	var y = 1;
+	
+	it('Repeater locator', function(){
+		browser.get('http://juliemr.github.io/protractor-demo/');
+		browser.driver.manage().window().setPosition(x, y);
+		browser.driver.manage().window().setSize(width, height);
+		
+		element(by.model("first")).sendKeys("3");
+		element(by.model("second")).sendKeys("2");
+		element(by.id("gobutton")).click();
+		
+		element(by.model("first")).sendKeys("10");
+		element(by.model("second")).sendKeys("10");
+		element(by.id("gobutton")).click();
+		
+		element(by.model("first")).sendKeys("5");
+		element(by.model("second")).sendKeys("5");
+		element(by.id("gobutton")).click();
+		
+		element.all(by.repeater("result in memory")).count().then(function(text){
+			//on a repeater, count returns de number of data(rows), this makes 3 calculations will count to 3 then
+			//each needs to be resolved
+			console.log(text);
+		})
+		
+		element.all(by.repeater("result in memory")).each(function(item) {
+			item.element(by.css("td:nth-child(3)")).getText().then(function(text){
+				console.log(text);
+			})
+		})
+
+	})
+	
+})
--- ProtractorTutorial/configuration.js
+++ ProtractorTutorial/configuration.js
@@ -6,8 +6,9 @@ exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	
 	//specs: ['Specs/Spec1.js'],
-	specs: ['ElementBasics.js'],
+	//specs: ['ElementBasics.js'],
 	//specs: ['ChainLocators.js'],
+	specs: ['All.js'],
     
 	capabilities: {
 		'browserName': 'chrome'
