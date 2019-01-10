a8094079dc7e905a076a471909fec4c1476c7dba
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2019-01-09 21:13:43 -0300

    Dropdown Lesson

--- ProtractorTutorial/Dropdowns.js
+++ ProtractorTutorial/Dropdowns.js
@@ -4,36 +4,52 @@
 
 describe('Dropdowns lesson', function(){
 	
-	it('Dropdown Test 1', function(){
-		browser.get('http://juliemr.github.io/protractor-demo/');
-		browser.driver.manage().window().maximize();
-		
-		addition(2, 2);//4
-		addition(4, 2);//6
-		addition(5, 15);//20
-		addition(50, 50);//100
+	function calc(a,b,c){
+		element(by.model("first")).sendKeys(a);
+		element(by.model("second")).sendKeys(b);
 		
 		//getAttribute will get the content inside 'value' and will loop on EACH one of them
 		element.all(by.tagName("option")).each(function(item) {
 			item.getAttribute("value").then(function(values){
-				console.log(values);
+					if(values==c){
+						//item has the Element of the tagName "option", and using "all", can see each value of the option tag
+						item.click();
+						console.log("THE VALUE IS: "+values);
+					}
 				})
 			})
-		})
-		
-		
-	it('ADDITION Operation', function(){
-		
+		element(by.id("gobutton")).click();
+	}
+	
+	it('Dropdown Test 1', function(){
 		browser.get('http://juliemr.github.io/protractor-demo/');
 		browser.driver.manage().window().maximize();
 		
-		element(by.model("first")).sendKeys("3");
-		element(by.model("second")).sendKeys("2");
-		element(by.model("operator")).click().element(by.css("option:nth-child(1)[value='ADDITION']")).click();
-		element(by.id("gobutton")).click();
+		calc(3, 5, "MULTIPLICATION");
+		calc(3, 5, "DIVISION");
+		calc(3, 5, "DIVISION");
+		calc(10, 6, "ADDITION");
+
+		element.all(by.repeater("result in memory")).each(function(item) {
+			item.element(by.css("td:nth-child(3)")).getText().then(function(text){
+				console.log("Print:" + text);
+			})
+		})
+	})
 		
-		expect(element(by.css("h2[class='ng-binding']")).getText()).toBe("5");
-	})		
+		
+//	it('ADDITION Operation', function(){
+//		
+//		browser.get('http://juliemr.github.io/protractor-demo/');
+//		browser.driver.manage().window().maximize();
+//		
+//		element(by.model("first")).sendKeys("3");
+//		element(by.model("second")).sendKeys("2");
+//		element(by.model("operator")).click().element(by.css("option:nth-child(1)[value='ADDITION']")).click();
+//		element(by.id("gobutton")).click();
+//		
+//		expect(element(by.css("h2[class='ng-binding']")).getText()).toBe("5");
+//	})		
 
 })
 	
